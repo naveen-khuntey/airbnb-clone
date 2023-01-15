bdff1b183c61334fef5bc46518539d4265100d13
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2023-01-15 13:57:00 +0530

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Nav(){
+    return (
+        <div className="img-nav">
+            <div className="img-photo-1">
+                <img src="./images/airbnb-logo.png" alt="the cow" />
+            </div>
+            {/* <div className="img-photo-2">
+                <img src="./images/photo-grid.png" alt="the momo"/>
+            </div> */}
+        </div>
+    )
+}
